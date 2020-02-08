3b46176dccf71179b4452a7b053598bc7d512cc8
Hari Venu <hari.venu@rd.genero.jp>
2020-02-08 22:26:16 +0530

    Task 1.

---
+++ web/modules/custom/johns_module/src/EventSubscriber/CountryRedirectionSubscriber.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\johns_module\EventSubscriber;
+
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Session\SessionManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Cache\CacheableMetadata;
+
+/**
+ * Class CountryRedirectionSubscriber
+ *
+ *
+ * @package Drupal\johns_module\EventSubscriber
+ */
+class CountryRedirectionSubscriber implements EventSubscriberInterface {
+  
+  public function languageRedirection(GetResponseEvent $event) {
+  
+    global $base_url;
+    $ip = $event->getRequest()->getClientIp();
+
+    $ip = '51.158.123.7';
+
+    if(filter_var($ip, FILTER_VALIDATE_IP) && ($ip != '::1' || $ip != '127.0.0.1')) {
+      // Use JSON encoded string and converts 
+      // it into a PHP variable 
+      $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+      $lc = \Drupal::languageManager()->getCurrentLanguage()->getId();
+      if($ipdat->geoplugin_countryCode == 'IN' && $lc != 'hi') {
+        $response = new TrustedRedirectResponse($base_url . '/hi');
+        $response->addCacheableDependency((new CacheableMetadata())->setCacheMaxAge(0));
+        $event->setResponse($response);
+      }
+      if($ipdat->geoplugin_countryCode == 'PK'  && $lc != 'ur' ) {
+        $response = new TrustedRedirectResponse($base_url . '/ur');
+        $response->addCacheableDependency((new CacheableMetadata())->setCacheMaxAge(0));
+        $event->setResponse($response);
+      }
+      // other countries
+      if (!in_array($ipdat->geoplugin_countryCode, ['IN', 'PK']) && $lc != 'en') {
+        $response = new TrustedRedirectResponse($base_url . '/en');
+        $response->addCacheableDependency((new CacheableMetadata())->setCacheMaxAge(0));
+        $event->setResponse($response);
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // 'checkForRedirection' is the name of our function
+    $events[KernelEvents::REQUEST][] = array('languageRedirection');
+    return $events;
+  }
+}
